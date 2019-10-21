a314c3f3d216742e198c19b0a7e8038a0bd7e579
miskina <cookie20061@hotmail.fr>
2019-10-21 20:34:36 +0200

    new port

--- FRONT_NotPassito/src/http-constants.js
+++ FRONT_NotPassito/src/http-constants.js
@@ -1,14 +1,6 @@
-import axios from 'axios'
+import axios from 'axios';
 
-let baseURL
 
-if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-  baseURL = 'http://localhost:8080/'
-} else {
-  baseURL = 'http://api.example.com'
-}
-
-export const HTTP = axios.create(
-  {
-    baseURL: baseURL
-  })
+export const HTTP=axios.create({
+    baseURL:'http://localhost:4000'
+});
